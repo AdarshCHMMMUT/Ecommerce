1159a4eb86dbc906d317d2acfd42df451fcfbfec
AdarshCHMMMUT <97625989+AdarshCHMMMUT@users.noreply.github.com>
2025-06-03 03:15:19 +0530

    a lot of things

--- src/api/getAllCategores.js
+++ src/api/getAllCategores.js
@@ -1,15 +1,9 @@
 import axios from "axios";
-
-const BaseUrl = 'https://api.escuelajs.co/api/v1';
-
 export const getAllCategories = async () => {
-    const url = `${BaseUrl}/categories`;
     try {
-        const { data } = await axios.get(url);
-          // Consider changing this to console.debug if it's for debugging purposes
+        const { data } = await axios.get('https://thirdcopyback.vercel.app/api/user/category');
         return data;
     } catch (err) {
         console.error("Failed to fetch products:", err);
-        throw new Error('An error occurred while fetching the products. Please try again later.');
     }
 };
--- src/api/getAllProducts.js
+++ src/api/getAllProducts.js
@@ -1,14 +1,9 @@
 import axios from "axios";
-
-const BaseUrl = 'https://api.escuelajs.co/api/v1';
-
 export const getAllProducts = async () => {
-    const url = `${BaseUrl}/products`;
     try {
-        const { data } = await axios.get(url);
+        const { data } = await axios.get('https://thirdcopyback.vercel.app/api/user/items');
         return data;
     } catch (err) {
         console.error("Failed to fetch products:", err);
-        throw new Error('An error occurred while fetching the products. Please try again later.');
     }
 };
